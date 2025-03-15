9de8a9983f5e5532d6c2dded0d6bc3f5d31c3063
SophiaAdeline <yamahasbr.25@gmail.com>
2025-03-15 14:56:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function convertToHtml(text) {
     // Mengganti #, ##, ###, dan ** dengan tag HTML yang sesuai
     let html = text
         .replace(/^(#{1})\s*(.+)$/gm, '<h1>$2</h1>') // H1
-        .replace(/^(#{2})\s*(.+)$/gm, '<h2>$2</h2>') // H2
-        .replace(/^(#{3})\s*(.+)$/gm, '<h3>$2</h3>') // H3
+        .replace(/^(#{2})\s*(.+)$/gm, '<h2 style="text-align:left;">$2</h2>') // H2
+        .replace(/^(#{3})\s*(.+)$/gm, '<h3 style="text-align:left;">$2</h3>') // H3
         .replace(/\*\*(.+?)\*\*/g, '<b>$1</b>') // Bold
         .replace(/^\s*[-\d]+\.\s*(.+)$/gm, '<details><summary>$&</summary>$1</details>') // Toggle
         .replace(/^(?!<h[1-3]>|<b>|<details>)(.+)$/gm, '<p>$1</p>'); // Paragraf
